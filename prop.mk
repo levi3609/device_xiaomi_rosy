049281709e42cb2379205efafca51a0c897eb241
NFS86 <jarbull86@gmail.com>
2022-01-27 20:17:25 +0700

    rosy: prop: update dalvik.vm
    Signed-off-by: NFS86 <jarbull86@gmail.com>

--- prop.mk
+++ prop.mk
@@ -220,11 +220,19 @@ ro.vendor.qti.am.reschedule_service=true
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.heapstartsize=16m \
-dalvik.vm.heapgrowthlimit=192m \
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed \
+dalvik.vm.dex2oat64.enabled=true \
+debug.hwui.renderer=opengl \
+ro.sys.fw.dex2oat_thread_count=4 \
+dalvik.vm.boot-dex2oat-threads=8 \
+dalvik.vm.dex2oat-threads=4 \
+dalvik.vm.image-dex2oat-threads=4 \
+dalvik.vm.heapgrowthlimit=256m \
+dalvik.vm.heapstartsize=8m \
 dalvik.vm.heapsize=512m \
 dalvik.vm.heaptargetutilization=0.75 \
-dalvik.vm.heapminfree=4m \
+dalvik.vm.heapminfree=512k \
 dalvik.vm.heapmaxfree=8m
 
 # Perf
