831213e18769dddac0f26b08a265e509750f3add
levi3609 <nazmulhasan3609@gmail.com>
2022-02-09 04:12:07 +0000

    rosy: AwakenOS bringup

--- awaken_rosy.mk
+++ awaken_rosy.mk
@@ -9,10 +9,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common ArcanaOS stuff
-$(call inherit-product, vendor/aosp/common.mk)
-ARCANA_DEVICE := rosy
-ARCANA_MAINTAINER := NFS-Project
+# Inherit some common AwakenOS stuff
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
 # Pixel customization
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
@@ -23,7 +21,7 @@ TARGET_INCLUDE_LIVE_WALLPAPERS := false
 $(call inherit-product, device/xiaomi/rosy/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_rosy
+PRODUCT_NAME := awaken_rosy
 PRODUCT_DEVICE := rosy
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5
